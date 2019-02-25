9d69ad59c1e4c9db0b864ed83e018492ef0c3207
Erik Prananta <erikpprananta@gmail.com>
2019-02-25 18:17:33 +0000

    split teacher and student

--- IPC/views.py
+++ IPC/views.py
@@ -12,12 +12,22 @@ import datetime
 from .models import Institution, Attendance, DailyAttendance, AttendanceStatus, Announcement, Course, Result, ResultType, AssigntmentDeadline, ExamDate
 from .forms import NewAnnouncementForm, NewAttendanceForm, NewResultForm, NewResultMarkForm
 
-
 def course(request):
-    courses = Course.objects.all()
-    return render(request, 'course.html', {'courses': courses})
+    user = request.user
+    if user.groups.filter(name='Teacher').exists():
+        user_type = "teacher"
+    elif user.groups.filter(name='Student').exists():
+        user_type = "student"
+    courses = Course.objects.filter(courseCode__in=user.groups.values_list('name', flat=True))
+    return render(request, 'course.html', {'courses': courses,
+                                           'user_type': user_type,})
 
 def course_statistic(request, course_id):
+    user = request.user
+    if user.groups.filter(name='Teacher').exists():
+        user_type = "teacher"
+    elif user.groups.filter(name='Student').exists():
+        user_type = "student"
     results = get_list_or_404(Result.objects.order_by('-resultReturnedDate'), resultStudentId=request.user)
     dailyAttendances = get_list_or_404(DailyAttendance.objects.order_by('-id'), dailyAttendanceStudentId=request.user)
     attendances = get_list_or_404(Attendance.objects.order_by('-attendanceDate'))
@@ -25,11 +35,18 @@ def course_statistic(request, course_id):
                                               'results': results,
                                               'dailyAttendances': dailyAttendances,
                                               'attendances': attendances,
+                                              'user_type': user_type,
                                               'data': zip(dailyAttendances,attendances)})
 
 def course_announcement(request, course_id):
+    user = request.user
+    if user.groups.filter(name='Teacher').exists():
+        user_type = "teacher"
+    elif user.groups.filter(name='Student').exists():
+        user_type = "student"
     announcements = get_list_or_404(Announcement.objects.order_by('-announcementDate'), announcementCourse=course_id)
     return render(request, 'announcement.html', {'announcements': announcements,
+                                                 'user_type': user_type,
                                                  'course_id': course_id,})
 
 def create_course_announcement(request, course_id):
@@ -46,12 +63,28 @@ def create_course_announcement(request, course_id):
     else:
         form = NewAnnouncementForm()
     return render(request, 'create_announcement.html', {'course_id': course_id,
-                                                         'form': form})
+                                                        'form': form})
 
 def course_attendance(request, course_id):
-    attendances = get_list_or_404(Attendance.objects.order_by('-attendanceDate'), attendanceCourseId=course_id)
-    return render(request, 'attendance.html', {'attendances': attendances,
-                                                      'course_id': course_id,})
+    user = request.user
+    if user.groups.filter(name='Teacher').exists():
+        user_type = "teacher"
+    elif user.groups.filter(name='Student').exists():
+        user_type = "student"
+    if user_type == "teacher":
+        attendances = get_list_or_404(Attendance.objects.order_by('-attendanceDate'), attendanceCourseId=course_id)
+        return render(request, 'attendance.html', {'attendances': attendances,
+                                                   'user_type': user_type,
+                                                   'course_id': course_id,})
+    elif user_type == "student":
+        dailyAttendances = get_list_or_404(DailyAttendance.objects.order_by('-id'), dailyAttendanceStudentId=request.user)
+        attendances = get_list_or_404(Attendance.objects.order_by('-attendanceDate'))
+        return render(request, 'attendance.html', {'dailyAttendances': dailyAttendances,
+                                                   'attendances': attendances,
+                                                   'user_type': user_type,
+                                                   'course_id': course_id,
+                                                   'data': zip(dailyAttendances,attendances)})
+
 
 def create_course_attendance(request, course_id):
     course = get_object_or_404(Course, id=course_id)
@@ -82,9 +115,29 @@ def course_attendance_detail(request, course_id, attendance_id):
                                                       'course_id': course_id,})
 
 def course_result(request, course_id):
-    results = get_list_or_404(Result.objects.order_by('-resultReturnedDate'), resultStudentId=request.user)
-    return render(request, 'result.html', {'results': results,
-                                            'course_id': course_id,})
+    user = request.user
+    if user.groups.filter(name='Teacher').exists():
+        user_type = "teacher"
+    elif user.groups.filter(name='Student').exists():
+        user_type = "student"
+    if user_type == "student":
+        results = get_list_or_404(Result.objects.order_by('-resultReturnedDate'), resultStudentId=request.user)
+        return render(request, 'result.html', {'results': results,
+                                               'user_type': user_type,
+                                               'course_id': course_id,})
+    elif user_type == "teacher":
+        name = Result.objects.order_by('-pk')[0]
+        results = get_list_or_404(Result.objects.order_by('-resultReturnedDate'), resultStudentId=name.resultStudentId)
+        print(results)
+        return render(request, 'result.html', {'results': results,
+                                               'user_type': user_type,
+                                               'course_id': course_id,})
+
+def course_result_detail(request, course_id, result_id):
+    result_name = get_object_or_404(Result, id=result_id)
+    results = get_list_or_404(Result.objects.order_by('resultName'), resultName=result_name.resultName)
+    return render(request, 'result_detail.html', {'results': results,
+                                                  'course_id': course_id,})
 
 def post_course_result(request, course_id):
     course = get_object_or_404(Course, id=course_id)
@@ -109,14 +162,20 @@ def post_course_result(request, course_id):
         form = NewResultForm()
         formset = studentMarkFormSet()
     return render(request, 'create_result.html', {'students': students,
-                                                      'course_id': course_id,
-                                                      'formset': formset,
-                                                      'form': form,
-                                                      'data': zip(students, formset)})
+                                                  'course_id': course_id,
+                                                  'formset': formset,
+                                                  'form': form,
+                                                  'data': zip(students, formset)})
 
 def course_calendar(request, course_id):
+    user = request.user
+    if user.groups.filter(name='Teacher').exists():
+        user_type = "teacher"
+    elif user.groups.filter(name='Student').exists():
+        user_type = "student"
     exams = get_list_or_404(ExamDate.objects.order_by('-examDateDate'), examDateCourse=course_id)
     assigntments = get_list_or_404(AssigntmentDeadline.objects.order_by('-assigntmentDeadlineDueDate'), assigntmentDeadlineCourse=course_id)
     return render(request, 'calendar.html', {'exams': exams,
-                                            'assigntments': assigntments,
-                                            'course_id': course_id,})
+                                             'assigntments': assigntments,
+                                             'user_type': user_type,
+                                             'course_id': course_id,})
